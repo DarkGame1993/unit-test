ca1a961b9b8efdbb3a9e98df632e79b0af46e587
Varerii Skorokhod <valeriiskorokhod@gmail.com>
2022-01-12 12:52:52 +0200

    Eventemitter test

--- src/app/counter/counter.component.spec.ts
+++ src/app/counter/counter.component.spec.ts
@@ -17,4 +17,12 @@ describe('CounterComponent', () => {
         component.decrement();
         expect(component.counter).toBe(-1) 
     })
+
+    it('should increment value by event emitter', () => {
+        let result: any = null
+        component.counterEmitter.subscribe(v => result = v);
+        component.increment()
+
+        expect(result).toBe(1)
+    })
 })
